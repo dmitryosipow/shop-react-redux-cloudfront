325955bdf4563f57ca53ee3cb9dfa6fea230666d
Dmitry_Osipov <dmitry_osipov@epam.com>
2023-07-29 00:23:32 +0200

    Task 5 - add import file path and uncomment getting signed url functionality

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://guvp6nveuk.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://yw459sxtei.execute-api.eu-west-1.amazonaws.com/",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
