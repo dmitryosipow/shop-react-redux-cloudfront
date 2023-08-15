8d849c33a49217bc5366811c16715e96944693a1
Dmitry_Osipov <dmitry_osipov@epam.com>
2023-08-15 19:10:27 +0200

    Task 7 - Show notification when getting 401 and 403 response errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import toast, { Toaster } from "react-hot-toast";
 import App from "~/components/App/App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
@@ -7,6 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +21,21 @@ const queryClient = new QueryClient({
   worker.start({ onUnhandledRequest: "bypass" });
 }*/
 
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log(error.response);
+    if (error.response.status == 401 || error.response.status == 403) {
+      toast.error(
+        `Error ${error.response.status} ${error.response.data?.message}`
+      );
+    }
+    return Promise.reject(error);
+  }
+);
+
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
@@ -27,6 +44,7 @@ root.render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
+          <Toaster />
           <CssBaseline />
           <App />
         </ThemeProvider>
